5c95b484c784c258f10fe65c92eb4a253fac2d8a
Kim Haagen Mathiesen <kimhmathiesen@gmail.com>
2025-06-23 13:12:39 +0200

    requirement, readme and more

--- cv_rag.py
+++ cv_rag.py
@@ -6,16 +6,16 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 import os
 
-# Model til embedding af 
-embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2", 
-                                   local_files_only=True)
-# LLM til generering af tekst
+# Model for embedding
+embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# LLM for generating text
 llm = Ollama(model="gemma2:9b")
 
-# Database placering
+# Database location
 db_path = "./chroma_db"
 
-# Hvis Choma database eksistere
+# If ChromaDB already exists
 if os.path.exists(db_path):
     print("ChromaDB already exists. Reusing existing database.")
     db = chromadb.PersistentClient(path=db_path)
@@ -26,22 +26,29 @@ if os.path.exists(db_path):
         vector_store=vector_store,
         embed_model=embed_model
     )
-# Hvis Chroma database ikke eksistere, så opret den.
+# If ChromaDB does not exist, create it
 else:
     print("ChromaDB does not exist. Creating new database.")
     documents = SimpleDirectoryReader("./data").load_data()
+    print(f"Loaded {len(documents)} documents")
+
     db = chromadb.PersistentClient(path=db_path)
     chroma_collection = db.get_or_create_collection("quickstart")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
     index = VectorStoreIndex.from_documents(
-        documents, 
+        documents,  
         storage_context=storage_context, 
         embed_model=embed_model
     )
 
-# Opret query engine
-query_engine = index.as_query_engine(llm=llm)
-# Hent svar fra LLM
-response = query_engine.query("Skriv en ansøgning ud jobopslaget, med udgangspunkt i mit CV. Skriv i samme tone. Præsenter dig som Gemma2 der skriver ansøgningen på mine vegne, forklar du er implementeret i Python og koden kan findes på følgende github link: https://github.com/kimhaagenmathiesen/cv_rag/blob/main/data/cv.pdf.")
+# Create query engine
+query_engine = index.as_query_engine(llm=llm, verbose=True)
+# Get response from LLM
+response = query_engine.query("Write a job application based on my CV for the AI Software Engineer position.")
 print(response)
+
+# Retrieve and preview context nodes
+retriever = index.as_retriever()
+results = retriever.retrieve("Write a job application based on my CV for the AI Software Engineer position.")
+
